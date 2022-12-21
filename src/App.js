e7459ed78461d991e8ff0231ddc97c955d28a50f
andersonhsporto <anderson.higo2@gmail.com>
2022-12-20 23:20:12 -0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Form from "./page/form";
+
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" exact element={<Form/>}/>
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
